48278cd67fa8d59b7c7cea9ea67c0cf012939a7b
Tinotsu <tinopiof@gmail.com>
2025-11-15 19:07:39 +0100

    test to modify csv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import csv
 import cmd
 
 def main():
@@ -18,6 +19,7 @@ def get_files_info(directory="."):
 class MyCLI(cmd.Cmd):
     prompt = 'GFS ~ '
     intro = 'Welcome to GFS. Type "help" for available commands.'
+    file = 'GFS.csv'
 
     def do_hello(self, line):
         """Print a greeting."""
@@ -25,6 +27,24 @@ class MyCLI(cmd.Cmd):
 
     def do_help(self, line):
         print("This is an application to manage your files like a graph system.")
+
+    def do_print(self, arg):
+        with open(self.file, 'r') as f:
+            for line in f:
+                if arg == 'all':
+                    print(line)
+
+    def do_tag(self, tag):
+        with open(self.file, 'r') as f:
+            for line in f:
+                if ('t$ ' + tag) == line:
+                    print(f't$ {tag} is a tag')
+                    break
+                else:
+                    with open(self.file, 'a') as f:
+                        writer = csv.writer(f)
+                        writer.writerow(['t$ '+tag])
+                    break
 
     def do_quit(self, line):
         """Exit the CLI."""
